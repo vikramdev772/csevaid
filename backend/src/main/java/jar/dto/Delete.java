36845c5f48488ade0c3fd4f4b1584bfab8e94a9b
Santhoshini-7 <kachamsanthoshini@gmail.com>
2026-08-04 14:32:35 +0530

    Added delete API endpoint

---
+++ backend/src/main/java/jar/dto/Delete.java
@@ -0,0 +1,17 @@
+package jar.dto;
+
+public class Delete {
+
+    private long id;
+
+    public Delete() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
