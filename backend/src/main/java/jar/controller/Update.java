101534d3746fdcaa18373b7be592796c6884e9ad
jeevanareddyyerra <jeevanareddyyerra@gmail.com>
2026-08-04 11:16:34 +0530

    Added Update controller

---
+++ backend/src/main/java/jar/controller/Update.java
@@ -0,0 +1,53 @@
+package jar.controller;
+
+import java.util.*;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jar.model.Student;
+import jar.repo.StudentRepo;
+
+@RestController
+@RequestMapping("/api")
+public class Update {
+
+    @Autowired
+    StudentRepo db;
+
+    @PutMapping("/update/{id}")
+    public Map<Object, Object> m1(@RequestBody Student s, @PathVariable int id) {
+
+        Map<Object, Object> res = new HashMap<>();
+
+        Optional<Student> data = db.findById(id);
+
+        if (data.isPresent()) {
+
+            Student obj = data.get();
+
+            obj.setName(s.getName());
+            obj.setEmail(s.getEmail());
+            obj.setIp(s.getIp());
+
+            db.save(obj);
+
+            res.put("msg", "Student updated successfully");
+            res.put("status", 200);
+            res.put("student", obj);
+
+        } else {
+
+            res.put("msg", "Student not found");
+            res.put("status", 404);
+
+        }
+
+        return res;
+    }
+}
